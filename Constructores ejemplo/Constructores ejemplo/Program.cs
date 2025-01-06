5c226057c32d2ad232be4405624a92b755c67c2d
Gonzalo Bertolucci <gonzalobertolucci.gb@gmail.com>
2025-01-06 10:51:08 -0300

    uso de "this" en el metodo set. explicacion de "partial"

--- Constructores ejemplo/Constructores ejemplo/Program.cs
+++ Constructores ejemplo/Constructores ejemplo/Program.cs
@@ -10,16 +10,22 @@
             Coche coche2 = new Coche();
 
             Console.WriteLine(coche1.getInfoCoche());
+            Console.WriteLine(coche1.getExtras());
             //Instanciacion de objeto coche con valores por constructor.
             //Diferente estado inicial
+            Console.WriteLine("\n");
+
             Coche coche3 = new Coche(1500.8, 0.500);
-
             Console.WriteLine(coche3.getInfoCoche());
-
+            coche3.setExtras(true, "cuero");
+            Console.WriteLine(coche3.getExtras());
             
+
         }
     }
-
+    //C# permite dividir mis clases. Se debe colocar "partial" al inicio
+    //Ejemplo: partial class Coche
+    //luego cerrar las llaves donde quiero dividir la clase
     class Coche
     {
         //Constructor, sirve para dar un estado inicial
@@ -28,6 +34,7 @@
             ruedas = 4;
             largo = 2300.5;
             ancho = 0.800;
+            tapiceria = "tela";
         }
         //Sobrecarga de constructor
         public Coche(double largoCoche, double anchoCoche)
@@ -37,6 +44,8 @@
             largo = largoCoche;
 
             ancho = anchoCoche;
+
+            tapiceria = "tela";
         }
 
 
@@ -44,6 +53,20 @@
         public String getInfoCoche()
         {
             return "Informacion del coche \n" + "Ruedas: " + ruedas + "\nLargo: " + largo+ "\nAncho: " + ancho;
+        }
+        //Setter
+        public void setExtras(bool climatizador, String tapiceria)
+        {
+            //Si se genera un problema de ambiguedad y el nombre de mis parametros son iguales a los de mis propiedades
+            //Utilizo el 'this' el cual hace referencia a este campo de clase. Donde me encuentro actualmente y no la propiedad.
+            this.climatizador = climatizador;
+
+            this.tapiceria = tapiceria; 
+        }
+        
+        public String getExtras()
+        {
+            return "Extras del coche\n" + "Tapiceria: " + tapiceria + "\nClimatizador: " + climatizador;
         }
 
         private int ruedas;
