1e8350b53b4e2969dc6f3868be2eb5338210e7df
Gonzalo Bertolucci <gonzalobertolucci.gb@gmail.com>
2025-01-06 10:13:12 -0300

    Se agregan nuevas lineas y ejemplo de accesos a una propiedad

--- Program.cs
+++ Program.cs
@@ -6,12 +6,18 @@
         {
             Circulo miCirculo = new Circulo(); //Creacion de objeto de tipo ciruclo. Iniciacion de variable/objeto.
 
-            Console.WriteLine(miCirculo.calculoArea(5));
+            Console.WriteLine(miCirculo.CalculoArea(5));
 
             Circulo miCirculo2 = new Circulo();
 
-            Console.WriteLine(miCirculo2.calculoArea(9));
+            Console.WriteLine(miCirculo2.CalculoArea(9));
 
+            conversorDolarPesos obj = new conversorDolarPesos();
+
+            obj.cambiarValorDolar(1080);
+
+            Console.WriteLine(obj.convierte(50));
+            Console.ReadLine();
         }
     }
 }
@@ -22,8 +28,32 @@ class Circulo
 
     private const double pi = 3.1416; //Propiedad de la clase circulo
 
-    public double calculoArea(int radio) //Metodo de clase.
+    public double CalculoArea(int radio) //Metodo de clase.
     {
         return pi * radio * radio;
+    }
+}
+
+class conversorDolarPesos
+{
+    //Propiedad encapsulada.
+    private double dolar = 1015;
+
+    public double convierte(double cantidad)
+    {
+        return dolar * cantidad;
+    }
+    //Si necesito cambiar el valor de un dato encapsulado lo mejor seria hacer esto.
+    public void cambiarValorDolar(double nuevoValor)
+    {
+        if(nuevoValor > 0)
+        {
+            dolar = nuevoValor;
+        }
+        else
+        {
+            Console.WriteLine("Error. Debe introducir un numero positivo");
+        }
+        
     }
 }
