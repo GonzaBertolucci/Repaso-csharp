03041a2e31dd8926fca0ca020809e51f5b57b168
Gonzalo Bertolucci <114008639+GonzaBertolucci@users.noreply.github.com>
2025-01-07 09:02:03 -0300

    POO

---
+++ PracticaPOO/Constructores ejemplo/Constructores ejemplo/Program.cs
@@ -0,0 +1,82 @@
+﻿namespace Constructores_ejemplo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Coche coche1 = new Coche(); //Crear objeto de tipo coche.
+            //Dar un estado inicial a nuestro coche.
+
+            Coche coche2 = new Coche();
+
+            Console.WriteLine(coche1.getInfoCoche());
+            Console.WriteLine(coche1.getExtras());
+            //Instanciacion de objeto coche con valores por constructor.
+            //Diferente estado inicial
+            Console.WriteLine("\n");
+
+            Coche coche3 = new Coche(1500.8, 0.500);
+            Console.WriteLine(coche3.getInfoCoche());
+            coche3.setExtras(true, "cuero");
+            Console.WriteLine(coche3.getExtras());
+            
+
+        }
+    }
+    //C# permite dividir mis clases. Se debe colocar "partial" al inicio
+    //Ejemplo: partial class Coche
+    //luego cerrar las llaves donde quiero dividir la clase
+    class Coche
+    {
+        //Constructor, sirve para dar un estado inicial
+        public Coche()
+        {
+            ruedas = 4;
+            largo = 2300.5;
+            ancho = 0.800;
+            tapiceria = "tela";
+        }
+        //Sobrecarga de constructor
+        public Coche(double largoCoche, double anchoCoche)
+        {
+            ruedas = 4;
+
+            largo = largoCoche;
+
+            ancho = anchoCoche;
+
+            tapiceria = "tela";
+        }
+
+
+        //Getter
+        public String getInfoCoche()
+        {
+            return "Informacion del coche \n" + "Ruedas: " + ruedas + "\nLargo: " + largo+ "\nAncho: " + ancho;
+        }
+        //Setter
+        public void setExtras(bool climatizador, String tapiceria)
+        {
+            //Si se genera un problema de ambiguedad y el nombre de mis parametros son iguales a los de mis propiedades
+            //Utilizo el 'this' el cual hace referencia a este campo de clase. Donde me encuentro actualmente y no la propiedad.
+            this.climatizador = climatizador;
+
+            this.tapiceria = tapiceria; 
+        }
+        
+        public String getExtras()
+        {
+            return "Extras del coche\n" + "Tapiceria: " + tapiceria + "\nClimatizador: " + climatizador;
+        }
+
+        private int ruedas;
+
+        private double largo;
+
+        private double ancho;
+
+        private bool climatizador;
+
+        private String tapiceria;
+    }
+}
---
+++ PracticaPOO/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace PracticaPOO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circulo miCirculo = new Circulo(); //Creacion de objeto de tipo ciruclo. Iniciacion de variable/objeto.
+
+            Console.WriteLine(miCirculo.CalculoArea(5));
+
+            Circulo miCirculo2 = new Circulo();
+
+            Console.WriteLine(miCirculo2.CalculoArea(9));
+
+            conversorDolarPesos obj = new conversorDolarPesos();
+
+            obj.cambiarValorDolar(1080);
+
+            Console.WriteLine(obj.convierte(50));
+            Console.ReadLine();
+        }
+    }
+}
+
+
+class Circulo
+{
+
+    private const double pi = 3.1416; //Propiedad de la clase circulo
+
+    public double CalculoArea(int radio) //Metodo de clase.
+    {
+        return pi * radio * radio;
+    }
+}
+
+class conversorDolarPesos
+{
+    //Propiedad encapsulada.
+    private double dolar = 1015;
+
+    public double convierte(double cantidad)
+    {
+        return dolar * cantidad;
+    }
+    //Si necesito cambiar el valor de un dato encapsulado lo mejor seria hacer esto.
+    public void cambiarValorDolar(double nuevoValor)
+    {
+        if(nuevoValor > 0)
+        {
+            dolar = nuevoValor;
+        }
+        else
+        {
+            Console.WriteLine("Error. Debe introducir un numero positivo");
+        }
+        
+    }
+}
